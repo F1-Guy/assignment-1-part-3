9a42de945095b7a9a8aaf3e08f26477aa267e263
Bartol Cagalj <bartol.cagalj@gmail.com>
2022-09-23 12:43:02 +0200

    Basic implementation Add - ui Add - script to replicate a string based on user input

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 Vue.createApp({
     data() {
         return {
-            message: 'Hello World!'
-        }
-    }
-}).mount('#app')
+            string: null,
+            times: null,
+            result: null,
+        };
+    },
+
+    methods: {
+        replicate() {
+            if (this.times > 0) {
+                this.result = this.string.repeat(this.times);
+            }
+            else if (this.times == 0) {
+                this.result = "You can't replicate a string 0 times.";
+            }
+            else {
+                let reversed
+                reversed = this.string.split('').reverse().join('');
+                this.result = "I see what you tried to do there!\n" + reversed.repeat(Math.abs(this.times));
+            }
+        },
+    },
+}).mount("#app");
