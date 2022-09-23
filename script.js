dc1a8190ee766f5f880d63d9631a31e3fa890423
Bartol Cagalj <bartol.cagalj@gmail.com>
2022-09-23 13:00:11 +0200

    Modified error checking Change - way errors are displayed

--- script.js
+++ script.js
@@ -13,9 +13,10 @@ Vue.createApp({
                 this.result = this.string.repeat(this.times);
             }
             else if (this.times == 0) {
-                this.result = "You can't replicate a string 0 times.";
+                alert("You can't replicate a string 0 times.");
             }
             else {
+                alert("Times must be a positive integer. And it can't be " + this.times);
                 let reversed
                 reversed = this.string.split('').reverse().join('');
                 this.result = "I see what you tried to do there!\n" + reversed.repeat(Math.abs(this.times));
